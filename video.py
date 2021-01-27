98027cff0e9724a94a4197e7de59ac8cebb4c6a9
Lucas Leal <leallucasfonseca@gmail.com>
2021-01-27 13:24:32 -0300

    feat: Adiciona tratamento id inexistente e retorno status code

--- video.py
+++ video.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, abort
 
 videos = {}
 
@@ -7,10 +7,16 @@ video_put_args.add_argument("title", type=str, help="Needs a title for the Video
 video_put_args.add_argument("views", type=int, help="Views of the Video")
 video_put_args.add_argument("likes", type=int, help="Likes on the Video")
 
+def check_id_exist(video_id):
+    if video_id not in videos:
+        abort(404, message="Video not found...")
+
 class Video(Resource):
     def get(self, video_id):
+        check_id_exist(video_id)
         return videos[video_id]
 
     def put(self, video_id):
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return videos[video_id], 201 #Created
