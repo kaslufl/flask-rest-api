c8d9f9bbd355c2d62cccb4a933f5433325adca1b
kaslufl <leallucasfonseca@gmail.com>
2021-01-26 21:32:39 -0300

    feat: Adiciona resource na main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
+from hello_world import HelloWorld
 
 app = Flask(__name__)
 api = Api(app)
+
+api.add_resource(HelloWorld, "/helloworld")
 
 if __name__ == "__main__":
     app.run(debug=True)
