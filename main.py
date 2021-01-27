9bb2afd2a9916ab7b76c8bf795376c8b5915c082
Lucas Leal <leallucasfonseca@gmail.com>
2021-01-27 10:26:13 -0300

    feat: Adiciona resource Video na api

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from hello_world import HelloWorld
+from video import Video
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorld, "/helloworld")
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
